6a6202ffa33edb85d2f1916b657f2ca2545efe28
abenavidezUM <a.benavidez@alumno.um.edu.ar>
2024-10-04 22:26:51 -0300

    added setup_pieces

--- board.py
+++ board.py
@@ -24,3 +24,31 @@ class Board:
 
         if not for_test:
             self.setup_pieces()
+
+    def setup_pieces(self):
+        """
+        Sets up all the chess pieces on the board in their initial positions.
+        """
+        # Black pieces
+        self.positions[0][0] = Rook("black", (0, 0))
+        self.positions[0][1] = Knight("black", (0, 1))
+        self.positions[0][2] = Bishop("black", (0, 2))
+        self.positions[0][3] = Queen("black", (0, 3))
+        self.positions[0][4] = King("black", (0, 4))
+        self.positions[0][5] = Bishop("black", (0, 5))
+        self.positions[0][6] = Knight("black", (0, 6))
+        self.positions[0][7] = Rook("black", (0, 7))
+
+        for i in range(8):
+            self.positions[1][i] = Pawn("black", (1, i))
+            self.positions[6][i] = Pawn("white", (6, i))
+
+        # White pieces
+        self.positions[7][0] = Rook("white", (7, 0))
+        self.positions[7][1] = Knight("white", (7, 1))
+        self.positions[7][2] = Bishop("white", (7, 2))
+        self.positions[7][3] = Queen("white", (7, 3))
+        self.positions[7][4] = King("white", (7, 4))
+        self.positions[7][5] = Bishop("white", (7, 5))
+        self.positions[7][6] = Knight("white", (7, 6))
+        self.positions[7][7] = Rook("white", (7, 7))
