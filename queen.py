b867bf08660bc90d03367684d2fd887ca136bef4
abenavidezUM <a.benavidez@alumno.um.edu.ar>
2024-10-07 23:21:36 -0300

    Complete queen logic

--- queen.py
+++ queen.py
@@ -1,11 +1,45 @@
+from piece import Piece
 
-from enums import Player
-from rook import Rook
-from bishop import Bishop
+class Queen(Piece):
+    """
+    Represents a queen chess piece, inheriting from the Piece base class.
+    """
 
+    def __init__(self, color, position):
+        """
+        Initializes a Queen instance with a color and position.
 
-class Queen(Rook, Bishop):
-    # Get moves
-    def get_valid_piece_moves(self, game_state):
-        return (Rook.get_valid_piece_moves(Rook(self.get_name(), self.get_row_number(), self.get_col_number(), self.get_player()), game_state) +
-                Bishop.get_valid_piece_moves(Bishop(self.get_name(), self.get_row_number(), self.get_col_number(), self.get_player()), game_state))
+        Parameters:
+            color (str): The color of the queen, e.g., "white" or "black".
+            position (tuple): The current position of the queen on the board.
+        """
+        super().__init__(color, position)
+
+    def __str__(self):
+        """
+        Returns the Unicode character representing the queen, depending on its color.
+
+        Returns:
+            str: "♕" if the queen is white, "♛" if the queen is black.
+        """
+        return "♕" if self.__color__ == "white" else "♛"
+
+    def check_move(self, positions, new_position):
+        """
+        Checks if moving to new_position is a valid move for the queen.
+
+        Parameters:
+            positions (list): The current state of the board.
+            new_position (tuple): The position to move to.
+
+        Returns:
+            bool: True if the move is valid, False otherwise.
+        """
+        destination_piece = positions[new_position[0]][new_position[1]]
+        if destination_piece is not None and destination_piece.__color__ == self.__color__:
+            return False
+        return (
+            self.diagonal_move(positions, new_position) or
+            self.horizontal_move(positions, new_position) or
+            self.vertical_move(positions, new_position)
+        )
