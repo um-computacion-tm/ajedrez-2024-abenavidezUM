e5833d18ae282e781ab7049b34bf1ca0c4bc1339
abenavidezUM <a.benavidez@alumno.um.edu.ar>
2024-10-11 19:20:04 -0300

    refactor rook

--- rook.py
+++ rook.py
@@ -5,20 +5,37 @@ class Rook(Piece):
         return "♖" if self.color == "white" else "♜"
 
     def check_move(self, positions, new_position):
-        new_x, new_y, current_x, current_y = self.get_coordinates(new_position)
+        if not self.is_valid_direction(new_position):
+            return False
 
-        if new_x == current_x or new_y == current_y:
-            if new_x == current_x:
-                direction = 1 if new_y > current_y else -1
-                for y in range(current_y + direction, new_y, direction):
-                    if positions[new_x][y] is not None:
-                        return False
-            else:
-                direction = 1 if new_x > current_x else -1
-                for x in range(current_x + direction, new_x, direction):
-                    if positions[x][new_y] is not None:
-                        return False
-            destination_piece = positions[new_x][new_y]
-            if destination_piece is None or destination_piece.color != self.color:
-                return True
-        return False
+        if not self.is_path_clear(positions, new_position):
+            return False
+
+        return self.is_destination_valid(positions, new_position)
+
+    def is_valid_direction(self, new_position):
+        new_x, new_y, current_x, current_y = self.get_coordinates(new_position)
+        # Movimiento válido si es en la misma fila o columna
+        return new_x == current_x or new_y == current_y
+
+    def is_path_clear(self, positions, new_position):
+        new_x, new_y, current_x, current_y = self.get_coordinates(new_position)
+        if new_x == current_x:
+            # Movimiento vertical
+            direction = 1 if new_y > current_y else -1
+            for y in range(current_y + direction, new_y, direction):
+                if positions[new_x][y] is not None:
+                    return False
+        else:
+            # Movimiento horizontal
+            direction = 1 if new_x > current_x else -1
+            for x in range(current_x + direction, new_x, direction):
+                if positions[x][new_y] is not None:
+                    return False
+        return True
+
+    def is_destination_valid(self, positions, new_position):
+        new_x, new_y = new_position
+        destination_piece = positions[new_x][new_y]
+        # El destino es válido si está vacío o tiene una pieza oponente
+        return destination_piece is None or destination_piece.color != self.color
