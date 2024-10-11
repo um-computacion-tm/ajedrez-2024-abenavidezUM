f3a6ef5468410efb2be5a91040beebb624fdbe0e
abenavidezUM <a.benavidez@alumno.um.edu.ar>
2024-10-11 12:40:37 -0300

    refactor test

--- test/test_king.py
+++ test/test_king.py
@@ -1,44 +1,27 @@
-import unittest
+from test_piece import TestPiece
 from king import King
-from moves import MovePieceInvalid
 
-class TestKing(unittest.TestCase):
+class TestKing(TestPiece):
     def setUp(self):
-        self.king = King("white", (4, 4))
-        self.positions = [[None for _ in range(8)] for _ in range(8)]
-        self.positions[4][4] = self.king
-
-    def test_valid_moves(self):
-        # Test all valid moves around the king
-        valid_moves = [
+        super().setUp()
+        self.piece = King("white", (4, 4))
+        self.positions[4][4] = self.piece
+        
+        # Movimientos válidos para el rey
+        self.valid_moves = [
             (3, 3), (3, 4), (3, 5),
             (4, 3),         (4, 5),
             (5, 3), (5, 4), (5, 5)
         ]
-        for move in valid_moves:
-            with self.subTest(move=move):
-                self.assertTrue(self.king.check_move(self.positions, move))
-
-    def test_invalid_moves(self):
-        # Moves more than one square away
-        invalid_moves = [
+        
+        # Movimientos inválidos para el rey
+        self.invalid_moves = [
             (2, 4), (4, 2), (6, 4), (4, 6)
         ]
-        for move in invalid_moves:
-            with self.subTest(move=move):
-                self.assertFalse(self.king.check_move(self.positions, move))
-
-    def test_capture_opponent_piece(self):
-        # Place an opponent's piece adjacent to the king
-        opponent_piece = King("black", (3, 4))
-        self.positions[3][4] = opponent_piece
-        self.assertTrue(self.king.check_move(self.positions, (3, 4)))
-
-    def test_blocked_by_friendly_piece(self):
-        # Place a friendly piece adjacent to the king
-        friendly_piece = King("white", (3, 4))
-        self.positions[3][4] = friendly_piece
-        self.assertFalse(self.king.check_move(self.positions, (3, 4)))
-
-if __name__ == '__main__':
-    unittest.main()
+        
+        # Configuración para pruebas de captura
+        self.opponent_piece_type = King
+        self.opponent_piece_position = (3, 4)
+        
+        self.friendly_piece_type = King
+        self.friendly_piece_position = (3, 4)
